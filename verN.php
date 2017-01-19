9fa775a40fae27ef537d1f06398adcfb45dd3911
Fran López <fclopezjurado@gmail.com>
2017-01-19 22:56:33 +0100

    Fixes in "verN.php" and "verN2.php" PHP scripts to solve security problems with SQL injection

--- verN.php
+++ verN.php
@@ -1,10 +1,15 @@
 <?php
 include ("includes/abrirbd.php");
 
-$sql = "SELECT * FROM productos WHERE nombre ='{$_GET['nombre']}'";
-if (!$resultado = mysqli_query($link, $sql)) {
-    header("Location:error.html");
-    exit;
+if (array_key_exists('nombre', $_GET) && is_string($_GET['nombre'])
+    && (preg_match('/^[A-z0-9-_\s]+$/', $_GET['nombre']) === 1)) {
+    $nombre = mysqli_real_escape_string($link, $_GET['nombre']);
+    $sql    = "SELECT * FROM productos WHERE nombre ='{$nombre}'";
+
+    if (!$resultado = mysqli_query($link, $sql)) {
+        header("Location:error.html");
+        exit;
+    }
 }
 ?>
 <html>
@@ -15,7 +20,7 @@ if (!$resultado = mysqli_query($link, $sql)) {
     <body>
     <center>
         <?php
-        if (mysqli_num_rows($resultado) > 0) {
+        if (isset($resultado) && mysqli_num_rows($resultado) > 0) {
             echo "<table>";
             while ($prod = mysqli_fetch_assoc($resultado)) {
                 echo "<tr><td><img src='Imagenes/{$prod['imagen']}.jpg' width=100 height=200></td>";
@@ -23,7 +28,7 @@ if (!$resultado = mysqli_query($link, $sql)) {
             }
             echo "</table>";
         } else {
-            echo ("No existe el producto");
+            echo 'Producto incorrecto';
         }
 
         mysqli_close($link);
--- verN2.php
+++ verN2.php
@@ -1,32 +1,42 @@
 <?php
 include ("includes/abrirbd.php");
 
-$sql = "SELECT * FROM productos WHERE nombre ='{$_GET['nombre']}'";
-if (!$resultado = mysqli_query($link, $sql)) {
-    header("Location:error.html");
-    exit;
+if (array_key_exists('nombre', $_GET) && is_string($_GET['nombre'])
+    && (preg_match('/^[A-z0-9-_\s]+$/', $_GET['nombre']) === 1)) {
+    $conexion   = new mysqli('localhost', 'root', '', 'tienda');
+    $nombre     = mysqli_real_escape_string($conexion, $_GET['nombre']);
+    $sentencia  = $conexion->prepare ('SELECT * FROM productos WHERE nombre = ?');
+
+    $sentencia->bind_param('s', $nombre);
+    $sentencia->execute();
+    $resultado = $sentencia->get_result();
+
+    if (!$resultado) {
+        header("Location:error.html");
+        exit;
+    }
 }
 ?>
 <html>
-    <head> 
-        <title> Ver Productos </title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-    <center>
-        <?php
-        if (mysqli_num_rows($resultado) > 0) {
-            echo "<table>";
-            while ($prod = mysqli_fetch_assoc($resultado)) {
-                echo "<tr><td><img src='Imagenes/{$prod['imagen']}.jpg' width=100 height=200></td>";
-                echo "<td> precio={$prod['precio']}<br>talla={$prod['talla']}</td></tr>";
-            }
-            echo "</table>";
-        } else {
-            echo ("No existe el producto");
+<head>
+    <title> Ver Productos </title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<center>
+    <?php
+    if (isset($resultado) && mysqli_num_rows($resultado) > 0) {
+        echo "<table>";
+        while ($prod = mysqli_fetch_assoc($resultado)) {
+            echo "<tr><td><img src='Imagenes/{$prod['imagen']}.jpg' width=100 height=200></td>";
+            echo "<td> precio={$prod['precio']}<br>talla={$prod['talla']}</td></tr>";
         }
+        echo "</table>";
+    } else {
+        echo 'Producto incorrecto';
+    }
 
-        mysqli_close($link);
-        ?>
-    </body>
+    mysqli_close($link);
+    ?>
+</body>
 </html>
